b113201f74b5fd0f1081f74662f0d0bf9e169579
rebecaoz <lrpmqrp@gmail.com>
2024-02-11 18:16:55 -0300

    create fizzbuz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+for(let i=1;i<=15;i++){
+    if(i%3==0 && i%5==0){
+        console.log("FizzBuzz");
+    }else if(i%3==0 || i%5==0){
+        if(i%3==0){
+            console.log("Fizz");
+        }else{
+            console.log("Buzz");
+        }
+    }else{
+        console.log(i);
+    }
+    
+}
